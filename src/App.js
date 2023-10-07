d090f3688f64547401510b5dc8ec65c3f9e50f40
Innocent Makusa <24973307+makusa-the@users.noreply.github.com>
2023-10-07 16:55:57 +0200

    Editted App.js and index.css

--- src/App.js
+++ src/App.js
@@ -1,57 +1,143 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
-import DarkModeToggle from './components/DarkModeToggle';
+import DarkModeToggle from "./components/DarkModeToggle";
+import ScrollToTop from "./components/ScrollToTop";
+import { pageVariants, pageTransition } from "./animations";
 
 export default function App() {
   const [darkMode, setDarkMode] = useState(
-    localStorage.getItem('darkMode') === 'true'
+    localStorage.getItem("darkMode") === "true"
   );
 
-  // Toggle dark mode and update the class on the body element
+  useEffect(() => {
+    const body = document.body;
+    if (darkMode) {
+      body.classList.add("dark-mode");
+    } else {
+      body.classList.remove("dark-mode");
+    }
+  }, [darkMode]);
+
   const toggleDarkMode = () => {
     const newDarkMode = !darkMode;
     setDarkMode(newDarkMode);
-    localStorage.setItem('darkMode', newDarkMode);
-    document.body.classList.toggle('dark-mode');
+    localStorage.setItem("darkMode", newDarkMode.toString());
   };
+
   return (
-    <div className={`App ${darkMode ? 'dark-mode' : ''}`}>
-    <header>
-        <DarkModeToggle toggleDarkMode={toggleDarkMode} />
-      </header>
+    <Router>
+      <ScrollToTop />
+      <div className={`App ${darkMode ? "dark-mode" : ""}`}>
+        <header>
+          <DarkModeToggle toggleDarkMode={toggleDarkMode} />
+        </header>
 
-      {/* Navigation Bar */}
-      <Navbar />
+        {/* Navigation Bar */}
+        <Navbar />
 
-      {/* Hero Section (Personal Introduction) */}
-      <About />
+        <Route
+          render={({ location }) => (
+            <AnimatePresence exitBeforeEnter>
+              <Switch location={location} key={location.pathname}>
+                {/* Home Page (Personal Introduction) */}
+                <Route
+                  path="/"
+                  exact
+                  render={() => (
+                    <motion.div
+                      initial="initial"
+                      animate="in"
+                      exit="out"
+                      variants={pageVariants}
+                      transition={pageTransition}
+                    >
+                      <About />
+                    </motion.div>
+                  )}
+                />
 
-      {/* Projects Section */}
-      <Projects />
+                {/* Projects Page */}
+                <Route
+                  path="/projects"
+                  render={() => (
+                    <motion.div
+                      initial="initial"
+                      animate="in"
+                      exit="out"
+                      variants={pageVariants}
+                      transition={pageTransition}
+                    >
+                      <Projects />
+                    </motion.div>
+                  )}
+                />
 
-      {/* Skills Section */}
-      <Skills />
+                {/* Skills Page */}
+                <Route
+                  path="/skills"
+                  render={() => (
+                    <motion.div
+                      initial="initial"
+                      animate="in"
+                      exit="out"
+                      variants={pageVariants}
+                      transition={pageTransition}
+                    >
+                      <Skills />
+                    </motion.div>
+                  )}
+                />
 
-      {/* Testimonials Section */}
-      <Testimonials />
+                {/* Testimonials Page */}
+                <Route
+                  path="/testimonials"
+                  render={() => (
+                    <motion.div
+                      initial="initial"
+                      animate="in"
+                      exit="out"
+                      variants={pageVariants}
+                      transition={pageTransition}
+                    >
+                      <Testimonials />
+                    </motion.div>
+                  )}
+                />
 
-      {/* Contact Section */}
-      <Contact />
+                {/* Contact Page */}
+                <Route
+                  path="/contact"
+                  render={() => (
+                    <motion.div
+                      initial="initial"
+                      animate="in"
+                      exit="out"
+                      variants={pageVariants}
+                      transition={pageTransition}
+                    >
+                      <Contact />
+                    </motion.div>
+                  )}
+                />
+              </Switch>
+            </AnimatePresence>
+          )}
+        />
 
-      {/* Additional Sections */}
-      {/* Add more sections here as needed, such as Blog, Awards, or Resume. */}
-
-      {/* Footer Section */}
-      <footer className="bg-gray-800 text-white py-6 text-center">
-        <div className="container mx-auto">
-          <p>&copy; {new Date().getFullYear()} Innocent Makusa. All rights reserved.</p>
-        </div>
-      </footer>
-    </div>
+        {/* Footer Section */}
+        <footer className="bg-gray-800 text-white py-6 text-center">
+          <div className="container mx-auto">
+            <p>&copy; {new Date().getFullYear()} Innocent Makusa. All rights reserved.</p>
+          </div>
+        </footer>
+      </div>
+    </Router>
   );
 }
