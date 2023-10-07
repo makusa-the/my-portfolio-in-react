f7e8163a9b32f387fc7e3f5141bc999e6f8080ff
Innocent Makusa <24973307+makusa-the@users.noreply.github.com>
2023-10-07 16:34:03 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
-import { ArrowRightIcon } from "@heroicons/react/20/solid";
+import { ArrowRightIcon } from "@heroicons/react/outline";
+import { Link } from "react-scroll";
+import DarkModeToggle from "./DarkModeToggle";
+import { useDarkMode } from "tailwindcss-dark-mode";
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode();
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
   return (
-    <nav className="bg-gradient-to-r from-blue-500 to-indigo-600 p-4 md:sticky top-0 z-10">
+    <nav className={`bg-gradient-to-r from-blue-500 to-indigo-600 p-4 md:sticky top-0 z-10`}>
       <div className="container mx-auto flex flex-wrap items-center justify-between">
         <button
           className="text-white text-xl font-medium md:hidden"
@@ -53,44 +57,67 @@ export default function Navbar() {
             isMenuOpen ? "block" : "hidden"
           } md:block md:flex md:items-center w-full md:w-auto`}
         >
-          <a
-            href="#about"
-            className="block md:inline-block text-white hover:text-yellow-400 md:mr-4 py-2 md:py-0"
+          <Link
+            to="about"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            className="block md:inline-block text-white hover:text-yellow-400 md:mr-4 py-2 md:py-0 cursor-pointer"
           >
             About
-          </a>
-          <a
-            href="#projects"
-            className="block md:inline-block text-white hover:text-yellow-400 md:mr-4 py-2 md:py-0"
+          </Link>
+          <Link
+            to="projects"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            className="block md:inline-block text-white hover:text-yellow-400 md:mr-4 py-2 md:py-0 cursor-pointer"
           >
             Projects
-          </a>
-          <a
-            href="#skills"
-            className="block md:inline-block text-white hover:text-yellow-400 md:mr-4 py-2 md:py-0"
+          </Link>
+          <Link
+            to="skills"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            className="block md:inline-block text-white hover:text-yellow-400 md:mr-4 py-2 md:py-0 cursor-pointer"
           >
             Skills
-          </a>
-          <a
-            href="#testimonials"
-            className="block md:inline-block text-white hover:text-yellow-400 md:mr-4 py-2 md:py-0"
+          </Link>
+          <Link
+            to="testimonials"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            className="block md:inline-block text-white hover:text-yellow-400 md:mr-4 py-2 md:py-0 cursor-pointer"
           >
             Testimonials
-          </a>
-          <a
-            href="#contact"
-            className="block md:inline-block text-white hover:text-yellow-400 md:mr-4 py-2 md:py-0"
+          </Link>
+          <Link
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            className="block md:inline-block text-white hover:text-yellow-400 md:mr-4 py-2 md:py-0 cursor-pointer"
           >
             Contact
+          </Link>
+        </div>
+        <div className="flex items-center space-x-4">
+          <DarkModeToggle darkMode={darkMode} setDarkMode={setDarkMode} />
+          <a
+            href="https://1drv.ms/w/s!Au_wnQIyOF5zgVM1FSUl4REIru5s?e=HUwhca"
+            className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0 text-white hover:text-yellow-400 transition duration-300"
+          >
+            My Resume
+            <ArrowRightIcon className="w-4 h-4 ml-1" />
           </a>
         </div>
-        <a
-          href="https://1drv.ms/w/s!Au_wnQIyOF5zgVM1FSUl4REIru5s?e=HUwhca"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
-        >
-          My Resume
-          <ArrowRightIcon className="w-4 h-4 ml-1" />
-        </a>
       </div>
     </nav>
   );
