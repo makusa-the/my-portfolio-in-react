1bca6e489111501d5014dac2e4d21401ea3bca9e
Innocent Makusa <24973307+makusa-the@users.noreply.github.com>
2023-10-07 16:30:22 +0200

    Updated Projects.js v2

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,64 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { motion } from "framer-motion";
-import { Link } from "react-scroll";
+import React, { useState } from "react";
+import { CodeBracketIcon } from "@heroicons/react/outline";
 import { projects } from "../data";
-import DarkModeToggle from "./DarkModeToggle";
-import { useDarkMode } from "tailwindcss-dark-mode";
+import { motion } from "framer-motion";
 
 export default function Projects() {
-  const [darkMode, setDarkMode] = useDarkMode();
-  const [isVisible, setIsVisible] = useState(false);
+  const [activeProjectIndex, setActiveProjectIndex] = useState(0);
 
-  useEffect(() => {
-    // Add scroll event listener to trigger animations
-    const handleScroll = () => {
-      if (window.scrollY > 100) {
-        setIsVisible(true);
-      } else {
-        setIsVisible(false);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  const variants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+  const handleProjectChange = (index) => {
+    setActiveProjectIndex(index);
   };
 
   return (
-    <section
-      id="projects"
-      className={`bg-gray-900 text-white py-16 ${
-        darkMode ? "dark-mode" : ""
-      }`}
-    >
+    <section id="projects" className="bg-gray-900 text-white py-16">
       <div className="container mx-auto text-center">
-        <motion.h1
-          className="text-4xl font-bold mb-6"
-          initial="hidden"
-          animate={isVisible ? "visible" : "hidden"}
-          variants={variants}
-        >
-          Projects Showcase
-        </motion.h1>
-        <motion.p
-          className="text-lg mb-12"
-          initial="hidden"
-          animate={isVisible ? "visible" : "hidden"}
-          variants={variants}
-        >
+        <CodeBracketIcon className="w-12 h-12 mx-auto mb-6 text-green-400" />
+        <h1 className="text-4xl font-bold mb-6">Projects Showcase</h1>
+        <p className="text-lg mb-12">
           Check out some of the exciting projects I've worked on.
-        </motion.p>
+        </p>
         <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-          {projects.map((project) => (
+          {projects.map((project, index) => (
             <motion.div
               key={project.title}
-              className="bg-gray-800 rounded-lg overflow-hidden shadow-lg"
-              whileHover={{ scale: 1.05 }}
+              className="relative bg-gray-800 rounded-lg overflow-hidden shadow-lg"
+              whileHover={{ scale: 1.03 }}
             >
               <img
                 src={project.image}
@@ -80,20 +45,21 @@ export default function Projects() {
                   </a>
                 </div>
               </div>
+              <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+                <button
+                  onClick={() => handleProjectChange(index)}
+                  className={`${
+                    index === activeProjectIndex
+                      ? "bg-blue-500"
+                      : "bg-gray-700"
+                  } px-4 py-2 rounded-full text-white`}
+                >
+                  {index === activeProjectIndex ? "Active" : "Switch"}
+                </button>
+              </div>
             </motion.div>
           ))}
         </div>
-        <div className="mt-8">
-          <Link
-            to="contact"
-            smooth={true}
-            duration={500}
-            className="text-blue-400 hover:text-blue-500 cursor-pointer"
-          >
-            Contact Me
-          </Link>
-        </div>
-        <DarkModeToggle darkMode={darkMode} setDarkMode={setDarkMode} />
       </div>
     </section>
   );
